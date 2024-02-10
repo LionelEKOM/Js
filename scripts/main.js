fc184591e6c3b585430b4c1e48a3cbeca9fd74c0
Lionel EKOM <ekomlionel@gmail.com>
2024-02-11 00:34:53 +0100

    Soumission des donnees d'un formulaire

--- scripts/main.js
+++ scripts/main.js
@@ -4,26 +4,21 @@
  * 
  *********************************************************************************/
 
-/*********************************************************************************
- * 
- * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
- * 
- *********************************************************************************/
+lancerJeu()
 
-// Ici, j'ai mis la fonction lancerJeu() en commentaire pour ne pas être dérangé par l'affichage des popups. 
-// lancerJeu()
+// J'ai mis ce code en commentaire, nous pourrons le récupérer lorsque nous en auront besoin :
 
-let inputEcriture = document.getElementById("inputEcriture")
-console.log(inputEcriture)
+// let inputEcriture = document.getElementById("inputEcriture")
+// console.log(inputEcriture)
 
-let btnValiderMot = document.getElementById("btnValiderMot")
-console.log(btnValiderMot)
+// let btnValiderMot = document.getElementById("btnValiderMot")
+// console.log(btnValiderMot)
 
-let zoneProposition = document.querySelector(".zoneProposition")
-console.log(zoneProposition)
+// let zoneProposition = document.querySelector(".zoneProposition")
+// console.log(zoneProposition)
 
-let spanScore = document.querySelector(".zoneScore span")
-console.log(spanScore)
+// let spanScore = document.querySelector(".zoneScore span")
+// console.log(spanScore)
 
-let listeBtnRadio = document.querySelectorAll(".optionSource input")
-console.log(listeBtnRadio)
+// let listeBtnRadio = document.querySelectorAll(".optionSource input")
+// console.log(listeBtnRadio)
